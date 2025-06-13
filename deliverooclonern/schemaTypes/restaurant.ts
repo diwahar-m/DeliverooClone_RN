ee1a56504cf3b922a3ac475bea65f14c5f8fbfbd
diwahar-m <msdiwahar5@gmail.com>
2025-06-13 11:56:50 +0530

    added necessary schemas for sanity

--- deliverooclonern/schemaTypes/restaurant.ts
+++ deliverooclonern/schemaTypes/restaurant.ts
@@ -1,65 +1,62 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'restaurant',
+  title: 'Restaurant',
   type: 'document',
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
+      name: 'name',
       type: 'string',
+      title: 'Restaurant name',
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
+      name: 'image',
       type: 'image',
-      options: {
-        hotspot: true,
-      },
+      title: 'Image of the Restaurant',
     }),
     defineField({
-      name: 'categories',
-      title: 'Categories',
+      name: 'lat',
+      type: 'number',
+      title: 'Latitude of the Restaurant',
+    }),
+    defineField({
+      name: 'long',
+      type: 'number',
+      title: 'Longitude of the Restaurant',
+    }),
+    defineField({
+      name: 'address',
+      type: 'string',
+      title: 'Restaurant address',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'rating',
+      type: 'number',
+      title: 'Enter a Rating from (1-5 Stars)',
+      validation: (Rule) =>
+        Rule.required().min(1).max(5).error('Please enter a Value between 1 and 5'),
+    }),
+    defineField({
+      name: 'type',
+      title: 'Category',
+      validation: (Rule) => Rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}],
+    }),
+    defineField({
+      name: 'dishes',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{type: 'dish'}]}],
     }),
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
 })
