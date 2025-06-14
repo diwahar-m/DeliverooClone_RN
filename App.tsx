0152090d9bcf0bbbdb999c9ff31668d05c43d664
diwahar-m <msdiwahar5@gmail.com>
2025-06-14 17:30:06 +0530

    Integrated food items with redux store

--- App.tsx
+++ App.tsx
@@ -6,16 +6,20 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import {store} from './store';
+import {Provider} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
