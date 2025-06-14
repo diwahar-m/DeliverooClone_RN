9d5e824fb1b8876d0288781dc6354bfa8ec26ae5
diwahar-m <msdiwahar5@gmail.com>
2025-06-14 16:34:08 +0530

    added Dish list of each restaurant

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -18,6 +18,7 @@ import {
   MapPinIcon,
   QuestionMarkCircleIcon,
 } from 'react-native-heroicons/outline';
+import DishRow from '../components/DishRow';
 
 const RestaurantScreen = () => {
   const {
@@ -81,13 +82,24 @@ const RestaurantScreen = () => {
         <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
           <QuestionMarkCircleIcon color="gray" opacity="0.6" size={20} />
           <Text className="pl-2 flex-1 text-md font-bold">
-            Have a foof allergy?
+            Have a food allergy?
           </Text>
           <ChevronRightIcon color="#00CCBB" />
         </TouchableOpacity>
       </View>
       <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl"></Text>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+        {/* DishRow */}
+        {dishes?.map(dish => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </ScrollView>
   );
